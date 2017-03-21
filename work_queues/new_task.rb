86ca670f10fd6a56386bd3458626bbc8f87e4873
Sebuuma Daniel <sedzsoft@gmail.com>
2017-03-21 18:19:42 +0300

    [WORK QUEUE] Adds prefetching, message persistance and durable queue.

--- work_queues/new_task.rb
+++ work_queues/new_task.rb
@@ -4,8 +4,10 @@ connection = Bunny.new
 connection.start
 
 channel = connection.create_channel
-q = channel.queue('hello')
+q = channel.queue('task_queue', :durable => true) #Can another queue name is required because hello was not initially
+                                                  # declared as durable. This makes RabbitMQ remember the queue in case
+                                                  # of failure or quiting
 
 msg = ARGV.empty? ? 'Hello World!' : ARGV.join(' ')
-q.publish(msg, :persistent => true)
+q.publish(msg, :persistent => true) #Ensures the messages will be persisted on RabbitMQ in case of restart or failure.
 puts " [x] Sent #{msg}"
--- work_queues/worker.rb
+++ work_queues/worker.rb
@@ -4,13 +4,19 @@ connection = Bunny.new
 connection.start
 
 channel = connection.create_channel
-q = channel.queue('hello')
+q = channel.queue('task_queue', :durable => true)
 
+channel.prefetch(1) # Ensures RabbitMQ only dispatches msgs to workers that aren't busy. 1 is the No. of messages
 puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
-q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
-  puts " [x] Received #{body}"
 
-  sleep body.count('.').to_i
-  puts " [x] Done"
-  channel.ack(delivery_info.delivery_tag)
+begin
+  q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+    puts " [x] Received #{body}"
+
+    sleep body.count('.').to_i
+    puts " [x] Done"
+    channel.ack(delivery_info.delivery_tag)
+  end
+rescue Interrupt => _
+  connection.close
 end
