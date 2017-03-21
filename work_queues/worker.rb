7b8a78f2cc3e3ac8df2e014b210ab3c6d0c7b356
Sebuuma Daniel <sedzsoft@gmail.com>
2017-03-21 18:03:10 +0300

    Adds acknowledgement feature

--- work_queues/worker.rb
+++ work_queues/worker.rb
@@ -7,9 +7,10 @@ channel = connection.create_channel
 q = channel.queue('hello')
 
 puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
-q.subscribe(:block => true) do |delivery_info, properties, body|
+q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
   puts " [x] Received #{body}"
 
   sleep body.count('.').to_i
-  puts " [x] Done in #{complete - start}"
+  puts " [x] Done"
+  channel.ack(delivery_info.delivery_tag)
 end
